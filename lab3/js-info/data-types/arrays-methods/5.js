a24917e65521f69e0c322a0a9be6d7bcff14a945
Lazynx <frichaplay@gmail.com>
2025-02-06 00:12:30 +0500

    [feat]: lab3 done

--- lab3/js-info/data-types/arrays-methods/5.js
+++ lab3/js-info/data-types/arrays-methods/5.js
@@ -1,10 +1,10 @@
 function copySorted(arr) {
-    return arr.slice().sort();
-  }
-  
-  let arr = ["HTML", "JavaScript", "CSS"];
-  
-  let sorted = copySorted(arr);
-  
-  alert( sorted );
-  alert( arr );
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted );
+alert( arr );
